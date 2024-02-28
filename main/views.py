a8874c282c402db1aa4872a20f4bc56ae463641f
Jasurbek <jasurkamilov7279@gmail.com>
2024-02-29 00:07:35 +0500

    .

--- main/views.py
+++ main/views.py
@@ -171,16 +171,7 @@ class UserTestResultsAPIView(APIView):
             )
             return Response({'error': 'Profile not found. Error ID: {}'.format(error_log.error_number)},
                             status=status.HTTP_404_NOT_FOUND)
-        except Exception as e:
-            error_code = str(uuid.uuid4())[:15]
-            error_log = main_models.ErrorLog.objects.create(
-                error_number=error_code,
-                error_message=str(e),
-                user_agent=request.META.get('HTTP_USER_AGENT'),
-                ip_address=request.META.get('REMOTE_ADDR'),
-            )
-            return Response({'error': 'An error occurred. Error ID: {}'.format(error_log.error_number)},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 # class PhotoAPIView(views.APIView):
 #     permission_classes = [AllowAny]
 #     pagination_class = CustomPagination
